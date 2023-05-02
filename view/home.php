a325537e4bc47c5e8e2b2d04688365ee4cc9508e
Leonardo Lima <leonardostlima@gmail.com>
2023-05-02 00:15:04 -0300

    initial commit

---
+++ view/home.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- booststrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css" integrity="sha512-ZnR2wlLbSbr8/c9AgLg3jQPAattCUImNsae6NHYnS9KrIwRdcY9DxFotXhNAKIKbAXlRnujIqUWoXXwqyFOeIQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- css -->
+    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- title -->
+    <title>Hosting Company</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg sticky-top shadow-sm bg-light">
+        <div class="container p-4">
+            <a class="navbar-brand d-block" href="home.php">
+                <img src="assets/img/logo.png" alt="Logo">
+            </a>        
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav fw-bold">
+                    <li class="nav-item mx-xl-5">
+                        <a class="nav-link" href="#">Our Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog&News</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="d-block d-sm-flex me-md-5">
+                <a href="#" class="fw-bold me-4 nav-link my-4 my-sm-0 account">Account</a>
+                <span class="me-4 text-secondary d-md-flex d-none">|</span>
+                <div class="whatsapp d-flex gap-2">
+                    <img src="assets/img/phone-icon.png" alt="Phone Icon">
+                    <a class="whatsapp-number nav-link fw-bold" href="https://web.whatsapp.com/send?phone=2349067322844" target="_blank">+2349067322844</a>
+                </div>
+            </div>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggler" aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>    
+    </nav>
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col-lg-6">
+                <ul class="d-flex m-0 p-0 gap-3 mb-3">
+                    <li class="nav-item text-black-50"><a href="#" class="nav-link fw-bold">Hosting</a></li>
+                    <li class="nav-item text-black-50"><a href="#" class="nav-link fw-bold">Domain</a></li>
+                    <li class="nav-item text-black-50"><a href="#" class="nav-link fw-bold">SEO</a></li>
+                    <li class="nav-item text-black-50"><a href="#" class="nav-link fw-bold">Email</a></li>
+                </ul>
+                <h1 class="fw-bold mb-3">Premium Web Hosting for Your Website</h1>
+                <p class="mb-5 text-secondary">Blazing fast web hosting for individuals and<br/> businesses of all sizes backed by 24x7x365 Support.</p>
+                <div>
+                    <button class="btn btn-red me-3">Create an Account</button>
+                    <button class="btn btn-border">Choose your plan</button>
+                </div>
+            </div>
+            <div class="col-lg-5 mt-lg-0 mt-5 text-center">
+                <img class="img-fluid" src="assets/img/primary-img.png" alt="">
+            </div>
+        </div>
+    </section>
+    <section class="container mt-5">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2 class="fs-1 fw-bold">True Cloud Web Hosting</h2>
+                <p>True Cloud Web Hosting All of the hosting packages we offer are deployed instantly on our SSD powered cloud. We don’t use dedicated servers that operate on single pieces of hardware. Our entire infrastructure is built to be reliable, secure, and scalable.</p>
+            </div>
+            <div class="col-lg-6 mt-4 mt-lg-0">
+                <div class="row">
+                    <div class="col-4 d-flex flex-column justify-content-between">
+                        <img src="assets/img/panel-orange-logo.png" alt="cPanel">
+                        <img class="mb-3"src="assets/img/secured-by-Imunify360-logo.png" alt="Secured by Imunity 360">
+                    </div>
+                    <div class="col-4 d-flex flex-column justify-content-between">
+                        <img src="assets/img/litespeed-logo.png" alt="Litespeed">
+                        <img src="assets/img/softaculous-logo.png" alt="Softaculous">
+                    </div>
+                    <div class="col-4 d-flex flex-column justify-content-between">
+                        <img src="assets/img/wordpress-logo.png" alt="Wordpress">
+                        <img class="mb-4 pb-2"src="assets/img/cloudflare-logo.png" alt="Cloudflare">
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="container mt-5 pt-0 pt-sm-5 text-center">
+        <div class="row">
+            <div class="col-lg-3">
+                <img class="m-auto" src="assets/img/clock-icon.png" alt="Clock Icon">
+                <h3 class="fs-5 fw-bold mt-3">99.9% Uptime</h3>
+                <p class="paragraph-small">We Keep Your Web build Online 24x7x365.</br> Downtime not only costs you lost visitors<br/> but also damages your reputation and<br/> search engine rankings.</p>
+            </div>
+            <div class="col-lg-3">
+                <img src="assets/img/cloud-icon.png" alt="Cloud Icon">
+                <h3 class="fs-5 fw-bold mt-3">Blazing Fast Web Hosting</h3>
+                <p class="paragraph-small">We Keep Your Web build Online 24x7x365.</br> Downtime not only costs you lost visitors<br/> but also damages your reputation and<br/> search engine rankings.</p>
+            </div>
+            <div class="col-lg-3">
+                <img src="assets/img/certificate-icon.png" alt="Certificate Icon">
+                <h3 class="fs-5 fw-bold mt-3">Free SSL Certificates</h3>
+                <p class="paragraph-small">We Keep Your Web build Online 24x7x365.</br> Downtime not only costs you lost visitors<br/> but also damages your reputation and<br/> search engine rankings.</p>
+            </div>
+            <div class="col-lg-3">
+                <img src="assets/img/support-icon.png" alt="Support Icon">
+                <h3 class="fs-5 fw-bold mt-3">24x7 Friendly Support</h3>
+                <p class="paragraph-small">We Keep Your Web build Online 24x7x365.</br> Downtime not only costs you lost visitors<br/> but also damages your reputation and<br/> search engine rankings.</p>
+            </div>
+        </div>
+    </section>
+    <section class="container cards-container">
+        <div class="row-lg d-lg-flex justify-content-center gap-4">
+            <div>
+                <div class="d-flex d-lg-block justify-content-center">
+                    <div class="position-relative">
+                        <img class="quots" src="assets/img/quots-icon.png">
+                        <h2 class="fs-1 fw-bold ms-0 ms-lg-5">Real Stories from<br/> Real Customers</h2>
+                        <p class="ms-5">Get inspired by these stories.</p>
+                    </div>
+                </div>
+                <div class="d-flex d-lg-block justify-content-center">
+                    <div class="shadow px-5 rounded-2 card-1 m-0 ms-lg-5 mt-5 mt-lg-0">
+                        <img src="assets/img/cli2.png" alt="CLI">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem</p>
+                        <div>
+                            <p class="m-0 mt-5 fw-bold">Floyd Miles</p>
+                            <p class="text-secondary">Vice President, CLI</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div>
+                <div class="d-flex d-lg-block justify-content-center">
+                    <div class="shadow px-5 rounded-2 card-2 mt-4 mt-lg-5 mb-4">
+                        <img src="assets/img/jpnl2.png" alt="JPNL">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem</p>
+                        <div>
+                            <p class="m-0 mt-3 fw-bold">Jane Cooper</p>
+                            <p class="text-secondary">CEO, JPNL</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="d-flex d-lg-block justify-content-center mb-5">
+                    <div class="shadow px-5 rounded-2 card-3">
+                        <img src="assets/img/leeveon2.png" alt="LeeveOn">
+                        <p>LemonWares saved our time in Hosting my company page.</p>
+                        <div>
+                            <p class="m-0 mt-4 fw-bold">Kristin Watson</p>
+                            <p class="text-secondary">Co-Founder, LeeveOn Branding</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <footer class="container-fluid">
+        <div class="container py-5 text-light">
+            <div class="row">
+                <div class="col-md-3">
+                    <img class="mb-3" src="assets/img/logo-white.png" alt="Logo Footer">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis, lectus magna fringilla urna, porttitor rhoncus dolor purus non enim praesent elem</p>
+                </div>
+                <div class="col-md-1"></div>
+                <div class="col-md-2 mt-3 mt-lg-0">
+                    <p class="fw-bold">Service</p>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Domain</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Shared Hosting</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cloud Hosting</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Private Hosting</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mt-3 mt-lg-0">
+                    <p class="fw-bold">Hosting</p>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Cheap Hosting</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hosting Wordpress</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Email Hosting</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Hosting Unlimited</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mt-3 mt-lg-0">
+                <p class="fw-bold">Company</p>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Career</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Contact Us</a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-md-2 mt-3 mt-lg-0">
+                <p class="fw-bold">Help</p>
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">FAQ</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Help support</a>
+                        </li>
+                    </ul>
+                </div>
+                <p class="fw-bold text-center mt-5 fst-italic">Code by Leonardo. Copyright 2023</p>
+            </div>                
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+</html>
